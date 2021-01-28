88e1e273fafa13685120fc31a818d1e877bec509
Luis Garcia Vicente <garcia.vicente@bootcamp-upgrade.com>
2021-01-28 20:20:30 +0100

    Se añade: [1] Menú a la navbar + [2] Variables para los colores + [3] Saludo, nombre y profesión

--- src/core/Navbar/Navbar.jsx
+++ src/core/Navbar/Navbar.jsx
@@ -10,7 +10,14 @@ const Navbar = ({isScrolling}) => {
 
     return (
         <div className={`navbar ${isScrolling > 20 ? "scrolling" : null}`}>
-            <div className="navbar-logo" onClick={toTheTop}>Your name</div>
+            <div className="navbar-logo" onClick={toTheTop}>Luis G. Vicente</div>
+            <div className="navbar__max-width">
+                <ul className="menu">
+                    <li className="menu__list"><a href="About" className="menu__link">Sobre mí</a></li>
+                    <li className="menu__list"><a href="Timeline" className="menu__link">Cronología</a></li>
+                    <li className="menu__list"><a href="Contact" className="menu__link" >Contacto</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/pages/cover/Cover.jsx
+++ src/pages/cover/Cover.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import "./Cover.scss";
 import coverVideo from  "../../assets/media/pexels-pcdesk.mp4";
-import Navbar from "../../core/Navbar/Navbar";
 
 const Cover = () => {
     return (
-        <>
-        
-        <div className="cover-container">
-        <Navbar/>
+        <div className="cover-container" id="cover">
             <video className="video" src={coverVideo} autoPlay loop muted/>
-            <h1 className="text-main">Name Last Name</h1>
-            <p className="text-secondary">Web developer (crear menú navegación)</p>
+            <div className="cover-content">
+                <p className="text-welcome">¡Bienvenidos! Mi nombre es</p>
+                <h1 className="text-main">Luis García Vicente</h1>
+                <p className="text-secondary">y soy <span className="text-job">Desarrollador Web</span></p>
+            </div>
         </div>
-        </>
     )
 }
 
