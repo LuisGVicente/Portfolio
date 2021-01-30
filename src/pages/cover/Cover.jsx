1b851b9fb964460c0cbb4ef492fc872795c92222
Luis Garcia Vicente <garcia.vicente@bootcamp-upgrade.com>
2021-01-30 10:51:55 +0100

    Se añade efecto react-typing en Cover

--- src/pages/cover/Cover.jsx
+++ src/pages/cover/Cover.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import "./Cover.scss";
 import coverVideo from  "../../assets/media/pexels-pcdesk.mp4";
+import Typical from 'react-typical';
 
 const Cover = () => {
+
+
     return (
         <div className="cover-container" id="cover">
             <video className="video" src={coverVideo} autoPlay loop muted/>
             <div className="cover-content">
                 <p className="text-welcome">¡Bienvenidos! Mi nombre es</p>
                 <h1 className="text-main">Luis García Vicente</h1>
-                <p className="text-secondary">y soy <span className="text-job">Desarrollador Web</span></p>
+                <p className="text-secondary">
+                <Typical
+                        className="text-job"
+                        loop={Infinity}
+                        wrapper="b"
+                        steps={[
+                            'Desarrollador Web.',
+                            1000,
+                            'JavaScript, CSS, MongoDB, Agile...',
+                            1000,
+                            'Gestor de Equipos.',
+                            1000,
+                            'En español, inglés y francés.',
+                            1000
+                        ]}
+                    /></p>
             </div>
         </div>
     )
